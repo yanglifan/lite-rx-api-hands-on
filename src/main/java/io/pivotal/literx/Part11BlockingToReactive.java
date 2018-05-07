0f30c9d10bfafc9bae65ab76ebe553e3a2a3942c
Yang Lifan <yanglifan@gmail.com>
2018-05-07 16:35:19 +0800

    Pass Part11BlockingToReactive.fluxToBlockingRepository

--- src/main/java/io/pivotal/literx/Part11BlockingToReactive.java
+++ src/main/java/io/pivotal/literx/Part11BlockingToReactive.java
@@ -34,7 +34,7 @@ public class Part11BlockingToReactive {
 
 	// TODO Insert users contained in the Flux parameter in the blocking repository using an elastic scheduler and return a Mono<Void> that signal the end of the operation
 	Mono<Void> fluxToBlockingRepository(Flux<User> flux, BlockingRepository<User> repository) {
-		return null;
+		return flux.publishOn(Schedulers.elastic()).doOnNext(repository::save).then();
 	}
 
 }
